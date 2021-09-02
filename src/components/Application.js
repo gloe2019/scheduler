176efd7e35c14c372900a02055cb53883c293289
gloe2019 <gloedem@gmail.com>
2021-09-02 17:09:31 -0400

    remove hardcoded days array, update days with data from axios request to /api/days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,28 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
 
 import "components/Application.scss";
 
 import DayList from "./DayList";
 
 import Appointment from "./Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -73,6 +57,14 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/days").then((res) => {
+      console.log("axios get >>>", res.data);
+      setDays(res.data);
+    });
+  }, []);
   const appointmentList = appointments.map((app) => (
     <Appointment key={app.id} {...app} />
   ));
