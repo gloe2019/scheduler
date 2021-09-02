c3c846dcdf5085d0d18ccb828808353b2fc7a8d6
gloe2019 <gloedem@gmail.com>
2021-09-01 23:53:45 -0400

    update variableName

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,12 +1,12 @@
 import React from "react";
-import classNames from "classnames";
+//import classNames from "classnames";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
   console.log(props);
   // const { day, days, setDay } = props;
   // console.log(day, days, setDay);
-  const test = props.days.map((day) => (
+  const listItem = props.days.map((day) => (
     <DayListItem
       key={day.id}
       {...day}
@@ -14,5 +14,5 @@ export default function DayList(props) {
       setDay={props.setDay}
     />
   ));
-  return <ul>{test}</ul>;
+  return <ul>{listItem}</ul>;
 }
