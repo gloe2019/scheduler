f1a99d9c8af4a2da29892bccfb5878b45fa289c6
gloe2019 <gloedem@gmail.com>
2021-09-01 23:52:37 -0400

    add InterviewerList component + styling

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import InterviewerListItem from "./InterviewerListItem";
+
+import "./InterviewerList.scss";
+
+//props:
+//interviewers = arr
+//interviewer = id/#
+//setInterviewer = fnc
+export default function InterviewerList(props) {
+  console.log(">>>IL", props);
+  const { interviewers, value, onChange } = props;
+  const listItem = interviewers.map((int) => (
+    <InterviewerListItem
+      key={int.id}
+      {...int}
+      selected={int.id === value}
+      setInterviewer={(event) => onChange(int.id)}
+    />
+  ));
+  return (
+    <section className='interviewers'>
+      <h4 className='interviewers__header text--light'>Interviewer</h4>
+      <ul className='interviewers__list'>{listItem}</ul>
+    </section>
+  );
+}
