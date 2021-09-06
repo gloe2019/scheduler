208b1da089f09ce745c0481bdf969c993c79d4e5
gloe2019 <gloedem@gmail.com>
2021-09-06 19:18:10 -0400

    cleanup...

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,8 +1,4 @@
-//selector - function that accepts state as an argument and returns data derived from that state..
-//selectors depend only on the arguments passed to them!
-
 export function getAppointmentsForDay(state, day) {
-  //return an array of appointments for the given day..
   const filteredDay = state.days.filter(
     (selectedDay) => selectedDay.name === day
   );
@@ -21,16 +17,13 @@ export function getAppointmentsForDay(state, day) {
 export function getInterviewersForDay(state, day) {
   const dayInterviewers = [];
   const foundDay = state.days.find((el) => el.name === day);
-  //console.log("day.name", day.name, "givenDay", givenDay); //no nested for loops
 
   if (!foundDay || foundDay.interviewers.length === 0) {
     return dayInterviewers; //check for false case first, return without executing code
   }
   for (const int of foundDay.interviewers) {
-    // console.log(state.interviewers[int]);
     dayInterviewers.push(state.interviewers[int]);
   }
-  // console.log(dayInterviewers);
   return dayInterviewers;
 }
 
