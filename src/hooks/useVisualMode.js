0d543b0154a9e22d76f4aa10b4cfa31d8354efb7
gloe2019 <gloedem@gmail.com>
2021-09-07 02:02:36 -0400

    cleanup...

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -10,19 +10,16 @@ const useVisualMode = (initial) => {
       setHistory((prev) => [...prev, mode]);
     }
     //history.push(mode) -- don't edit your state directly -- change history through setHistory
-    console.log("-----Transition\n", "mode", mode, "history", history);
   };
 
   const back = () => {
-    console.log("....back", mode, history);
+    //console.log("....back", mode, history);
     if (history.length > 1) {
       history.pop();
     }
     setMode(history[history.length - 1]);
   };
   return { mode, transition, back };
-
-  //another way to do this: take a slice of history -- slice from last - 1
 };
 
 export default useVisualMode;
