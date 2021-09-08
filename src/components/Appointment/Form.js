6d725b7149d34c56946f2f45c8eb5bbf22a2e592
gloe2019 <gloedem@gmail.com>
2021-09-07 23:38:45 -0400

    write failing tests to validate input on form submission, implement validation feature in Form component, confirm functionality with passing tests

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import Button from "components/Button";
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
   const reset = () => {
     setName("");
     setInterviewer(null);
@@ -14,6 +15,16 @@ export default function Form(props) {
     reset();
     props.onCancel();
   };
+
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    props.onSave(name, interviewer);
+  }
+
   return (
     <main className='appointment__card appointment__card--create'>
       <section className='appointment__card-left'>
@@ -25,9 +36,13 @@ export default function Form(props) {
             type='text'
             placeholder='Enter Student Name'
             onChange={(event) => setName(event.target.value)}
-            onSubmit={(event) => event.preventDefault()}
+            onSubmit={(event) => {
+              event.preventDefault();
+            }}
+            data-testid='student-name-input'
           />
         </form>
+        <section className='appointment__validation'>{error}</section>
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
@@ -42,7 +57,7 @@ export default function Form(props) {
           <Button
             confirm
             onClick={() => {
-              props.onSave(name, interviewer);
+              validate();
             }}
           >
             Save
