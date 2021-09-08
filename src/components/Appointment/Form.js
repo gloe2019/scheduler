b09039a42f2d29c405dfe66e7e366692753703e2
gloe2019 <gloedem@gmail.com>
2021-09-08 00:14:44 -0400

    implement tests to improve code coverage for the Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,29 +6,30 @@ export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
   const [error, setError] = useState("");
+
   const reset = () => {
     setName("");
     setInterviewer(null);
   };
+
   const cancel = () => {
-    // console.log("name", name);
     reset();
     props.onCancel();
   };
 
-  function validate() {
+  const validate = () => {
     if (name === "") {
       setError("Student name cannot be blank");
       return;
     }
-
+    setError("");
     props.onSave(name, interviewer);
-  }
+  };
 
   return (
     <main className='appointment__card appointment__card--create'>
       <section className='appointment__card-left'>
-        <form autoComplete='off'>
+        <form autoComplete='off' onSubmit={(event) => event.preventDefault()}>
           <input
             className='appointment__create-input text--semi-bold'
             name='name'
@@ -36,9 +37,6 @@ export default function Form(props) {
             type='text'
             placeholder='Enter Student Name'
             onChange={(event) => setName(event.target.value)}
-            onSubmit={(event) => {
-              event.preventDefault();
-            }}
             data-testid='student-name-input'
           />
         </form>
@@ -54,12 +52,7 @@ export default function Form(props) {
           <Button danger onClick={cancel}>
             Cancel
           </Button>
-          <Button
-            confirm
-            onClick={() => {
-              validate();
-            }}
-          >
+          <Button confirm onClick={validate}>
             Save
           </Button>
         </section>
